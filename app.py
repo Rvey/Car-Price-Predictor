312402f7e28fde01d4627fe52e8893caf7ecd633
Redouane Bouabana <rredouane342@gmail.com>
2022-11-03 14:21:54 +0100

    add authorization

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request,json
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
+import json
 import pickle
 import pandas as pd
 import numpy as np
 
+load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 model = pickle.load(open("CarPricePredict.pkl", "rb"))
 df = pd.read_csv("Cleaned_Car_data.csv")
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -17,27 +20,29 @@ def index():
     purchase_year = sorted(df["year"].unique(), reverse=True)
     gear_box = sorted(df["gear_box"].unique())
 
-    brands.insert(0, "Select Company")
-    purchase_year.insert(0, "Select Year")
-    gear_box.insert(0, "Select Fuel Type")
+    # purchase_year_clean = [int(float(year[:-1]) * 1000) for year in df["year"]]
 
-    return json(brands=brands, car_models=car_models , purchase_year=purchase_year , gear_box=gear_box)
+    return jsonify({"car_models": car_models, "brands": brands, "gear_box": gear_box})
+
 
 @app.route("/predict", methods=["POST"])
 @cross_origin(supports_credentials=True)
 def predict():
     brand = request.form.get('brand')
     car_model = request.form.get('model')
-    # purchase_year = request.form.get('purchase_year')
+    purchase_year = request.form.get('purchase_year')
     gear_box = request.form.get('gear_box')
-    # kms_driven = request.form.get('kms_driven')
+    energy = request.form.get('energy')
+    trim = request.form.get('trim')
 
-    prediction = model.predict(
-        pd.DataFrame(columns=['model', 'brand', 'gear_box'], data=np.array([car_model, brand, gear_box]).reshape(1, 3)))
-    # print(prediction)
+    if request.headers.get('Authorization') == os.environ["API_KEY"]:
 
-    return str(np.round(prediction[0], 2))
-    # return {"brand": brand, "car_model": car_model, "gear_box": gear_box}
+        prediction = model.predict(
+        pd.DataFrame(columns=['model', 'brand', 'gear_box', 'energy' , 'year' , 'trim'], data=np.array([car_model, brand, gear_box,energy,purchase_year,trim]).reshape(1, 6)))
+
+        return str(np.round(prediction[0], 2))
+    else:
+        return jsonify({"message": "Unauthorized"})
 
 
 if __name__ == "__main__":
